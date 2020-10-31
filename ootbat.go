b5e4b6be93a14dbee59a688c48cf566a6d644948
vesche <vesche@protonmail.com>
2020-10-31 11:42:41 -0500

    Fix with charging status

--- ootbat.go
+++ ootbat.go
@@ -1,34 +1,43 @@
 package main
 
 import (
-    "fmt"
-    "time"
-    "io/ioutil"
-    "os/exec"
-    "strconv"
-    "strings"
+	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func getBatteryPercentage() int {
-    data, _ := ioutil.ReadFile("/sys/class/power_supply/BAT0/capacity")
-    percentString := strings.TrimSuffix(string(data), "\n")
-    percentInt, _ := strconv.Atoi(percentString)
-    return percentInt
+	data, _ := ioutil.ReadFile("/sys/class/power_supply/BAT0/capacity")
+	percentString := strings.TrimSuffix(string(data), "\n")
+	percentInt, _ := strconv.Atoi(percentString)
+	return percentInt
+}
+
+func getBatteryStatus() string {
+	data, _ := ioutil.ReadFile("/sys/class/power_supply/BAT0/status")
+	statusString := strings.TrimSuffix(string(data), "\n")
+	return statusString
 }
 
 func main() {
-    for {
-        var batteryPercentage int
-        batteryPercentage = getBatteryPercentage()
+	for {
+		var batteryPercentage int
+		batteryPercentage = getBatteryPercentage()
 
-        if batteryPercentage < 5 {
-            cmd := exec.Command("/usr/bin/aplay", "/opt/ootbat/OOT_LowHealth.wav")
-            err := cmd.Run()
-            if err != nil {
-                fmt.Println(err)
-            }
-        }
+		var batteryStatus string
+		batteryStatus = getBatteryStatus()
 
-        time.Sleep(10 * time.Second)
-    }
+		if batteryPercentage < 5 && batteryStatus != "Charging" {
+			cmd := exec.Command("/usr/bin/aplay", "/opt/ootbat/OOT_LowHealth.wav")
+			err := cmd.Run()
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+
+		time.Sleep(10 * time.Second)
+	}
 }
